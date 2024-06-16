3a57f100aceb5c24b671abcb5143ed38c96443b5
Dennis <ehrh0023@umn.edu>
2024-06-16 12:23:40 -0500

    Added basic shop

---
+++ Scripts/ShopMenu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+signal open
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _open():
+	print('asdasd')
+	self.visible = true
+	
+func _close():
+	self.visible = false
